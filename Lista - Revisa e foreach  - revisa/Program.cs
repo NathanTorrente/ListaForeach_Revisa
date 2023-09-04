6620e510526e79a30197c0a013fb06a74e2090c0
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-09-04 14:14:51 -0400

    Segundo Commit

--- Lista - Revisa e foreach  - revisa/Program.cs
+++ Lista - Revisa e foreach  - revisa/Program.cs
@@ -7,7 +7,18 @@ class Program
         List<string> listastring = new List<string>();
         listastring.Add("Elias");
         listastring.Add("João"); // adiciona os valores como ultimo elementos - final da lista
-        listastring.Insert(1, "João"); // insert pega adiciona e coloca o elemento aonde voce quiser
+        listastring.Insert(1, "João"); // Adiciona um elemento em uma posição específica da lista, voce define.
+
+
+
+
+        /*O foreach percorre todos os elementos da lista, da primeira posição até a ultima
+         * para cada repetição, copia o endereço da posição atual da lista ou seja, na primeira repetição
+         * 
+         * na segunda repetição, str o valor da posição 1 da lista (João)
+         * 
+         * 
+         * */
 
             for (int i = 0; i < listastring.Count; i++) 
             {
@@ -17,6 +28,23 @@ class Program
             {
             Console.WriteLine(str);
             }
+
         //listastrin.Count ---- retorna o valor da lista
+        //for (int i = 0; i < listastring.Count; i++)
+       // {
+       //     Console.WriteLine(listastring[i]);
+       // }
+
+        List<int> listInt = new List<int>();
+        listInt.Add(1);
+        listInt.Add(3);
+        listInt.Insert(2,4);
+
+        foreach(int i in listInt)
+        {
+            Console.WriteLine(i);
+        }
+
+
     }
 }
