68b76731cd971c5b56c6682c8a7132980e142221
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-09-04 14:07:20 -0400

    One serviço

---
+++ Lista - Revisa e foreach  - revisa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> listastring = new List<string>();
+        listastring.Add("Elias");
+        listastring.Add("João"); // adiciona os valores como ultimo elementos - final da lista
+        listastring.Insert(1, "João"); // insert pega adiciona e coloca o elemento aonde voce quiser
+
+            for (int i = 0; i < listastring.Count; i++) 
+            {
+              Console.WriteLine(listastring[i]);
+            }
+        foreach(string str in listastring)
+            {
+            Console.WriteLine(str);
+            }
+        //listastrin.Count ---- retorna o valor da lista
+    }
+}
