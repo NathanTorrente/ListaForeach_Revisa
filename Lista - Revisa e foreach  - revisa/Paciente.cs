0290a3e944d971ee57cae69b7e52d9517ef4a528
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-09-04 15:27:58 -0400

    Quinto commit

---
+++ Lista - Revisa e foreach  - revisa/Paciente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista___Revisa_e_foreach____revisa
+{
+    public class Paciente
+    {
+
+        public int Id_paciente { get; set; }
+        public string nome { get; set; }    
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNac { get; set; }
+
+        public Paciente(int id_paciente, string nome, string cpf, string email, DateTime dataNac)
+        {
+            Id_paciente = id_paciente;
+            this.nome = nome;
+            Cpf = cpf;
+            Email = email;
+            DataNac = dataNac;
+        }
+    }
+}
--- Lista - Revisa e foreach  - revisa/Program.cs
+++ Lista - Revisa e foreach  - revisa/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using Lista___Revisa_e_foreach____revisa;
+using System;
 
 class Program
 {
     static void Main(string[] args)
+    {
+
+        ExemploLista1();
+        ExemploListaObejtos();
+
+    }
+    static void ExemploLista1()
     {
         List<string> listastring = new List<string>();
         listastring.Add("Elias");
@@ -11,7 +19,7 @@ class Program
                                        // listastring.RemoveAt(1);
                                        // listastring.Remove("João");
         listastring.Add("Ana Maria");
-      
+
 
         listastring.Sort(); //Comando responsável por ordenar a lista em ordem alfabética.
         /*O foreach percorre todos os elementos da lista, da primeira posição até a ultima
@@ -24,31 +32,31 @@ class Program
         bool contem = listastring.Contains("João");// Verifica se o elemento existe
         Console.WriteLine(contem);
 
-            for (int i = 0; i < listastring.Count; i++) 
-            {
-              Console.WriteLine(listastring[i]);
-            }
-        foreach(string str in listastring)
-            {
+        for (int i = 0; i < listastring.Count; i++)
+        {
+            Console.WriteLine(listastring[i]);
+        }
+        foreach (string str in listastring)
+        {
             Console.WriteLine(str);
-            }
+        }
 
         //listastrin.Count ---- retorna o valor da lista
         //for (int i = 0; i < listastring.Count; i++)
-       // {
-       //     Console.WriteLine(listastring[i]);
-       // }
+        // {
+        //     Console.WriteLine(listastring[i]);
+        // }
 
         List<int> listInt = new List<int>();
         listInt.Add(1);
         listInt.Add(3);
-        listInt.Insert(0,4);
+        listInt.Insert(0, 4);
         //List<int> list2 = listInt.OrderByDescending().ToList();
 
         //foreach(int i in list2)
-       // {
-       //     Console.WriteLine(i);
-       // }
+        // {
+        //     Console.WriteLine(i);
+        // }
         //liststring.Count retorna o tamanho da lista 
 
         //  for (int i = 0;i < listInt.Count;i++)
@@ -57,6 +65,32 @@ class Program
         //  }
         List<double> listadouble = new List<double> { 2.5, 3, 8.2 };
         listadouble.Add(1);
-
     }
+    static void ExemploListaObejtos()
+    {
+
+        List<Paciente> ListaPacientes = new List<Paciente>();
+
+       
+        Paciente p1 = new Paciente(1, "Elias", "5454", "Elias@gmail.com", new DateTime(1982, 07, 22));
+        Paciente p2 = new Paciente(1, "Fulano", "111", "Jorge@gmail.com", new DateTime(2000, 07, 22));
+        Paciente p3 = new Paciente(1, "Mario", "423", "Elias@gmail.com", new DateTime(1999, 07, 22));
+        Paciente p4 = new Paciente(1, "Cabrero", "232", "Elias@gmail.com", new DateTime(2008, 07, 22));
+        Paciente p5 = new Paciente(1, "Halerrandro", "053400", "Elias@gmail.com", new DateTime(1982, 07, 22));
+
+
+   
+        ListaPacientes.Add(p1);
+        ListaPacientes.Add(p2);
+        ListaPacientes.Add(p3);
+        ListaPacientes.Add(p4);
+        ListaPacientes.Add(p5);
+
+       foreach(Paciente p in ListaPacientes)
+        {
+            Console.WriteLine($"{p.nome} {p.Cpf} {p.Email} {p.DataNac} ");
+        }
+        
+    }
+
 }
